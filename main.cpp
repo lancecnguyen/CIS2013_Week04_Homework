a8db7f0e59ab01341e13bcfdbe63991c3cb33ebd
unknown <lance.nguyen@okstate.edu>
2019-02-19 23:08:27 -0600

    Overloading
    Based on program page 236

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-using namespace std;
+
+double hat(double, double);
+double jacket(double, double, int);
+double waist (double, double, int);
 
 
 int main(){
-	int h, w, age;
+	using namespace std;
+	double h, w;
+	int age;
+	char again = 'Y';
+	
 	cout << "Please enter your height in inches: ";
 	cin >> h;
 	cout << "Please enter your weight in pounds: ";

db9e16de72862dad23c906a442a02b5cd0cb75e2
unknown <lance.nguyen@okstate.edu>
2019-02-19 21:40:58 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int h, w, age;
+	cout << "Please enter your height in inches: ";
+	cin >> h;
+	cout << "Please enter your weight in pounds: ";
+	cin >> w;
+	cout << "Please enter your age: ";
+	cin >> age;
+	
+	
+	
+	
+	return 0;
+}
